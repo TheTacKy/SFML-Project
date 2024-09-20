d40fe3845ae3b3616bf5bc22a1fffb115131c36e
thetacky <ientran0822@gmail.com>
2024-09-20 13:49:24 -0700

    PhysicsSystem class

---
+++ LightYearsEngine/include/framework/PhysicsSystem.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <box2d/b2_world.h>
+
+#include "framework/Core.h"
+
+namespace ly
+{
+	class PhysicsSystem
+	{
+	public:
+		static PhysicsSystem& Get();
+	protected:
+		PhysicsSystem();
+	private:
+		static unique<PhysicsSystem> physicsSystem;
+		b2World mPhysicsWorld;
+		float mPhysicsScale;
+	};
+}
